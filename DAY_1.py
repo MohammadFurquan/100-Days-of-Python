e87e4677516fdb9b64be8bc42c0ef02d918c7cd3
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-01 00:02:57 +0530

    Day 1 of Python

---
+++ DAY_1.py
@@ -0,0 +1,18 @@
+print("Day 1: 01/09/2023")
+
+# Python is general purpose interpreted programmming language which is developed by "Guido van Rossum" in the Year in 1989 but was introduced 
+# in the year 1992 
+# Guido wanted to name the language unique and short he used to watch the comedy show named as "Monty Python" so he decided to name as "PYTHON"
+
+# **Features of Python**
+# Simple
+# Supports Multiple Paradiagm
+# Cross Platform
+# Compiled language
+# Extensible 
+# Huge Libraries
+# Open Source
+
+# We print using print() function in Python
+print("Hello World")
+
