fda58e522c2f68aee61c11cfb9c032653add70a1
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-02 00:11:15 +0530

    Day 2 of Python

---
+++ DAY_2.py
@@ -0,0 +1,12 @@
+print("Day 2: 02/09/2023")
+
+# DOMAINS WHERE PYTHON IS USED ARE AS FOLLOWS :
+# 1. Data Analytics
+# 2. Data Science
+# 3. Web Development
+# 4. App Development
+# 5. Automation
+# 6. Artificial Intelligence
+# 7. Machine Learning
+
+print(123455)
