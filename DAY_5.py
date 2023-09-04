41df33d0d8b6cf185d02fa345218c652686ecb12
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-04 23:45:47 +0530

    Day 5 of Python

---
+++ DAY_5.py
@@ -0,0 +1,29 @@
+print("Day 5: 04/09/2023")
+#Comments are the part of the program that the programmer does not want to execute rather the programmer uses it to either explain a block of code
+# or to avoid the execution of a specific part of code while testing
+
+# we have to use '#' for single line comment
+''' We have to use  triple single quote
+for 
+multiline commnet'''
+
+
+
+
+# ESCAPE SEQUENCE CHAR: A character that cannot directly be used in string so we use escape sequence character
+
+print("Harry is a good boy")
+print("This person is also a good boy")
+print("John is a good boy \n and he is intellignet")
+print("John is a \"good boy\" and he is intellignet")
+
+
+
+
+#PRINT: function is use to print the output on the output screen
+#print function has parameters 
+#1. sep=" " it is a separator it used to to separate the values
+#2. end=" " end parameter in the print function is used to add any string.
+print("Hey, Good Morning, How are you")
+print("Hey","Good Morning", "How are you", sep='-', end=" Yeah ")
+print("I did it")
