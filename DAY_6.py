9a346420e0e7f98e00f8122b8db062eb68b2cdf4
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-05 23:54:17 +0530

    Day 6 of Python

---
+++ DAY_6.py
@@ -0,0 +1,52 @@
+print("Day 5: 05/09/2023")
+
+# VARIABLES : it is like a container that holds data. A variable is like creating a placeholder in memeory and assigning it some value.
+
+
+a = 123456789
+print(a)
+
+b = "PythonCode"
+print(b)
+
+a1 = 9
+print(a + a1)
+
+print("The type of a is", type(a))
+
+
+# DATA TYPES: Data Type specifies the type of value a variable can store
+# 1. Numeric data: int, float, complex
+# 2. Text data: str
+# 3. Boolean data: True, False
+# 4. Sequenced data: list, tuple
+
+
+a = 1
+b = True
+c = None
+d = "Python"
+e = 4+5j
+f = 5.55
+
+print("The type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is ",type(c))
+print("The type of d is ",type(d))
+print("The type of e is ",type(e))
+print("The type of f is ",type(f))
+
+g = complex(8,2)
+print(g)
+
+
+list1 = [8,2.3,[-4,5],['apple','banna']]
+print(list1)
+
+tuple1 = (True, 1, 5.55, "apple")
+print(tuple1)
+
+dict1 = {"name":"Furquan","age":22}
+print(dict1)
+
+
