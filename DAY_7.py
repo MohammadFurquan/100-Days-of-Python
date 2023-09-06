2894fcc9d25f8a6c957c27f6297c3c0b9955b25c
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-06 22:56:42 +0530

    Day 7 of Python

---
+++ DAY_7.py
@@ -0,0 +1,23 @@
+print("Day 7: 06/09/2023")
+
+# OPERATORS : Python has different types of operators for different operations. To create a calculator we require arithmetic operators.OPERATORS
+
+# Arithmetic operators:
+#     +       Addition
+#     -       Substraction
+#     *       Multiplication
+#     /       Division
+#     %       Modulus
+#     //      Floor Division
+
+
+print(5 + 6)
+print(15 - 6)
+print(15 * 6)
+print(15 / 6)
+print(15 // 6)
+print(15 % 6)
+print(15 ** 6)
+
+
+
