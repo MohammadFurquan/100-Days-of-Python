9e4938eece6c5fa9a9986d45c727caa30563f8ec
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-07 23:45:13 +0530

    Day 8 of Python

---
+++ DAY_8.py
@@ -0,0 +1,20 @@
+print("Day 8: 07/09/2023")
+
+# EXERCISE 1 - Create a Calculator
+# Create a calculator capable of performing additon, substraction, multiplication and divison operations on two numbers. 
+# Your program should format the output in a readable manner!
+
+a = int(input("Enter first operand: "))
+b = int(input("Enter second operand: "))
+
+# a = 50
+# b = 3
+
+print("The value of",a,"+",3," is ",a+b)
+print("The value of",a,"-",3," is ",a-b)
+print("The value of",a,"*",3," is ",a*b)
+print("The value of",a,"/",3," is ",a/b)
+
+
+# Tip : Alt + Shift + downArrow for replication of selected part
+# Tip : Ctrl + Alt + up/down
