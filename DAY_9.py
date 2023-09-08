d26bfb513e506c88cabf3431c8eae57b24b203e6
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-08 23:57:32 +0530

    Day 9 of python

---
+++ DAY_9.py
@@ -0,0 +1,20 @@
+print("Day 9: 08/09/2023")
+
+# TYPECASTING : The conversion of one data type into the other data type is known as type casting in python.
+# Two Types of Typecasting:
+# 1.Explicit Conversion(Explicit type casting in python) Done via Developer
+# 2.Implicit Conversion(Implicit type casting in python) Done via Python
+
+
+
+
+a = "1"
+b = "2"
+print(a + b)
+
+print(int(a) + int(b))
+
+
+c = 1.9
+d = 8
+print(c + d)
