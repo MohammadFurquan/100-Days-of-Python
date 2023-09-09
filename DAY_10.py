f00789286aa9993210d8ccca6f621ad2ef0271b7
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-09 23:55:10 +0530

    Day 10 of Python

---
+++ DAY_10.py
@@ -0,0 +1,19 @@
+print("Day 10: 09/09/2023")
+
+
+# Input() : In python we can take user input directly by using input() function. 
+# The input function gives a return value as string/character hence we have to pass that into a variable
+
+a = input("Enter your name : ")
+print("My name is",a)
+
+
+x = input("Enter first number : ")
+y = input("Enter second number : ")
+print(x + y)
+print(int(x) + int(y))
+
+
+x = int(input("Enter first number : "))
+y = int(input("Enter second number : "))
+print(x + y)
