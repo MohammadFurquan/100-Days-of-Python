28ce2dcfb4f73b149ab2f50ba798762bf4521155
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-11 21:51:06 +0530

    Day 12 of Python

---
+++ DAY_12.py
@@ -0,0 +1,20 @@
+print("Day 12: 11/09/2023")
+
+# STRING SLICING AND OPERATIONS ON STRING
+names = "Furquan,Faizi"
+print(len(names))
+print(names[0:5])
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:4]) #including 0 but not 4
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[-3:-1])
+
+
+
+
+
