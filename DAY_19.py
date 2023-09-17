7df774e90602ddc10ebdf34e947351af2867ec2c
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-17 23:24:05 +0530

    Day 19 of Python

---
+++ DAY_19.py
@@ -0,0 +1,15 @@
+print("Day 19: 18/09/2023")
+
+# BREAK CONTINUE
+
+for i in range(12):
+    print(i)
+    if i== 9:
+        break
+    
+
+for i in range(15):
+    print(i)
+    if i ==10:
+        continue
+
