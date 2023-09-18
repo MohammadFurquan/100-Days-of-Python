762ebe696b32d115ad9fd9dc3f32d5340587908a
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-18 23:56:46 +0530

    Day 20 of Python

---
+++ DAY_20.py
@@ -0,0 +1,22 @@
+print("Day 20: 18/09/2023")
+
+# FUNCTION : WHEN WE WANT TO USE A BLOCK OF CODE MULTIPLE TIME SO WE USE FUNCTION WE WRAP UP THAT LINES OF CODE AND USE IT
+
+
+def func():
+    print("This is my first function")
+
+func()
+
+
+
+
+def add_num():
+    a = 10
+    b = 20
+    c = a + b
+    print("a + b = ",c)
+
+add_num()
+
+
