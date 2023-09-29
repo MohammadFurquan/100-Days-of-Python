146c5d5c99fb9944676098f1b54a931f8e30af8a
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-09-29 23:22:25 +0530

    Day 31 of Python

---
+++ DAY_31.py
@@ -0,0 +1,17 @@
+print("Day 31: 29/09/2023")
+
+# SET are unordered collection of data items. They store multiple items in a single variables. set items are separated by commas
+
+
+s = {2,4,2,6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+for i in info:
+    print(i)
+
+s1 = set()
+print(type(s1))
+
