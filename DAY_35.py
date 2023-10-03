454a95d3febaba6913b553fd6da799070503c697
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-10-03 22:55:50 +0530

    Day 35 of Python

---
+++ DAY_35.py
@@ -0,0 +1,26 @@
+print("Day 35: 3/10/2023")
+
+# Else with Loops
+
+for i in range(5):
+    print(i)
+
+else:
+    print("Sorry no i")
+
+
+# For - else - break
+
+for i in range(5):
+    print(i)
+    if i == 4:
+        break
+
+else:
+    print("Sorry no i")
+
+
+
+
+
+
