aadf22745e5c83aeb125a2aee406d4f270db82cb
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-10-12 23:27:34 +0530

    Day 44 of Python

---
+++ DAY_44.py
@@ -0,0 +1,16 @@
+print("Day 44: 12/10/2023")
+
+# importing in python is the process of loading code from a Python module into the current script.
+# This allows you to use the functions and variables defined in the module in your current script, as well as any additional modules that 
+# imported module may depend on
+
+
+import math
+result = math.sqrt(9)
+print(result)
+
+
+
+
+
+
