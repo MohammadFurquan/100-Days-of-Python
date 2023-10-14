26a20f5b8f933f713aad6ba9810db76672b2cf6f
MohammadFurquan <113228830+MohammadFurquan@users.noreply.github.com>
2023-10-14 23:56:37 +0530

    Day 46 of Python

---
+++ DAY_46.py
@@ -0,0 +1,8 @@
+print("Day 46: 14/10/2023")
+
+# OS module : it is a built in module, it provides functions for interacting with the operating system.
+# It allows you to perform a wide varitety  of tasks, such as reading and writing files interacting with file system
+
+
+
+
